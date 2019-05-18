22118d59512aedf608e3a8d5edd7e6281f3e5273
faisaldeveloper <faisal.shahzad786@gmail.com>
2019-05-18 14:35:38 +0500

    Addd new HTML template in the project

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import _ from 'lodash';
+import './style.scss';
 
 function component() {
     const element = document.createElement('div');
   
-    element.innerHTML = _.join(['Hellooooooo', 'webpack'], ' ');
+    element.innerHTML = _.join(['Hellooo', 'webpack'], ' ');
+    element.classList.add('hello');
   
     return element;
   }
   
-  document.body.appendChild(component());
+  //document.body.appendChild(component());
--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,26 @@
   const path = require('path');
   const HtmlWebpackPlugin = require('html-webpack-plugin');
+  const CleanWebpackPlugin = require('clean-webpack-plugin');
 
   module.exports = {
-    entry: './src/index.js',
+    mode: 'development',
+    entry: {
+      app: './src/index.js'
+    },
     output: {
       filename: 'bundle.js',
       path: path.resolve(__dirname, 'dist')
     },
     module: {
       rules: [
+        {
+          test: /\.s[ac]ss$/,
+          use: ['style-loader', 'css-loader', 'sass-loader']
+        },
+        {
+          test: /\.css$/,
+          use: ['style-loader', 'css-loader']
+        },
         {
           test: /\.html$/,
           use: [
@@ -21,9 +33,14 @@
       ]
     },
     plugins: [
+      new CleanWebpackPlugin(),
       new HtmlWebpackPlugin({
         template: "./src/index.html",
         filename: "./index.html"
       })
-    ]
+    ],
+    output: {
+      filename: '[name].bundle.js',
+      path: path.resolve(__dirname, 'dist')
+    }
   };
